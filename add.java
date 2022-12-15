74cdf746aee9df5a1bfb7587f4b23d869d89145b
Afia Ehsan <ehsanafia54@gmail.com>
2022-12-15 13:03:32 +0530

    removed message in add.java

--- add.java
+++ add.java
@@ -2,6 +2,6 @@ public class add{
     public static void main(String []args)
     {
         int a=5,b=6;
-        System.out.println("The Sum is "+ (a+b));
+        System.out.println(a+b);
     }
 }
